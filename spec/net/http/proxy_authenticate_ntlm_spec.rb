5db13318d3b49c6b0a97f406720f30880a793dda
labocho <labocho@penguinlab.jp>
2021-06-28 16:12:24 +0900

    add spec

--- spec/net/http/proxy_authenticate_ntlm_spec.rb
+++ spec/net/http/proxy_authenticate_ntlm_spec.rb
@@ -1,9 +1,33 @@
-RSpec.describe Net::Http::ProxyAuthenticateNTLM do
+RSpec.describe Net::HTTP::ProxyAuthenticateNTLM do
   it "has a version number" do
-    expect(Net::Http::ProxyAuthenticateNTLM::VERSION).not_to be nil
+    expect(Net::HTTP::ProxyAuthenticateNTLM::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  # http_proxy=http://PROXY_USER:PROXY_PASSWORD@PROXY_ADDRESS:PROXY_PASSWORD bin/rspec spec
+  context "http_proxy env provided" do
+    let(:proxy) { URI.parse(ENV.fetch("http_proxy")) }
+
+    before do
+      skip "http_proxy env was not provided" unless ENV["http_proxy"]
+      Net::HTTP::ProxyAuthenticateNTLM.enabled = true
+    end
+
+    it "should get from http://example.com" do
+      http = Net::HTTP.new("example.com", 80, proxy.hostname, proxy.port, proxy.user, proxy.password)
+      res = http.start do |h|
+        h.get("/")
+      end
+      expect(res.code).to eq "200"
+    end
+
+    it "should get from http://example.com" do
+      http = Net::HTTP.new("example.com", 443, proxy.hostname, proxy.port, proxy.user, proxy.password)
+      http.use_ssl = true
+      res = http.start do |h|
+        h.get("/")
+      end
+      expect(res.code).to eq "200"
+    end
+
   end
 end
